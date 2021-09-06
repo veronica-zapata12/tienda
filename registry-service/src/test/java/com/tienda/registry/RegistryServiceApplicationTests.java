3d9cb4274e2c43ddaa0a0523f81f649de92fac52
veronica-zapata <veronica.zapata@ceiba.com.co>
2021-09-06 10:25:56 -0500

    configuracion eureka sever y config

---
+++ registry-service/src/test/java/com/tienda/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
