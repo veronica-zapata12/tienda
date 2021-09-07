0a78c7208dee45ab447f179c8c0d89d01a0dd088
veronica-zapata12 <veritozapata12@gmail.com>
2021-09-07 09:36:05 -0500

    configuracion gategay

---
+++ gategay-service/src/test/java/com/tienda/gategay/GategayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.gategay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GategayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
